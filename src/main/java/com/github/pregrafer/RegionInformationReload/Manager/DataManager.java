6b10ea96fabfa1a33ebafce4037799d17fbe2e37
Kerwin <shijie.kerwin@qq.com>
2023-03-19 00:37:51 +0800

    code redo

--- src/main/java/com/github/pregrafer/RegionInformationReload/Manager/DataManager.java
+++ src/main/java/com/github/pregrafer/RegionInformationReload/Manager/DataManager.java
@@ -1,11 +1,14 @@
 package com.github.pregrafer.RegionInformationReload.Manager;
 
 import com.github.pregrafer.RegionInformationReload.Region.Region;
+import com.github.pregrafer.RegionInformationReload.Region.RegionDetials.BallRegion;
+import com.github.pregrafer.RegionInformationReload.Region.RegionDetials.CubeRegion;
+import com.github.pregrafer.RegionInformationReload.RegionInformationReload;
+import com.github.pregrafer.RegionInformationReload.Tool.Point;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class DataManager {
     private static final HashMap<String, String> playerRegionLoc = new HashMap<>();
@@ -13,21 +16,13 @@ public class DataManager {
     private static final HashMap<String, Integer> regionTasks = new HashMap<>();
     private static final HashMap<String, String> biomes = new HashMap<>();
     private static final Set<Region> regions = new HashSet<>();
+    private static final FileConfiguration config = RegionInformationReload.getInstance().getConfig();
     private static String pluginPrefix;
     private static String biomeSwitch;
     private static String regionSwitch;
     private static int biomeSpeed, regionSpeed;
-    private static AreaManager areaManager;
     private static List<String> biomeInfos;
     private static boolean biomeHighAccuracy;
-
-    public static AreaManager getAreaManager() {
-        return areaManager;
-    }
-
-    public static void setAreaManager(AreaManager areaManager) {
-        DataManager.areaManager = areaManager;
-    }
 
     public static String getBiomeSwitch() {
         return biomeSwitch;
@@ -120,5 +115,118 @@ public class DataManager {
 
     public static HashMap<String, Integer> getRegionTasks() {
         return regionTasks;
+    }
+
+    public static void loadData() {
+        ConfigurationSection settings = config.getConfigurationSection("Settings");
+        ConfigurationSection tips = config.getConfigurationSection("Tips");
+        setPluginPrefix(settings.getString("pluginPrefix", "&7[&aRegionInfoRe&7]"));
+        setBiomeSpeed(settings.getInt("biomeSpeed", 20));
+        setRegionSpeed(settings.getInt("regionSpeed", 20));
+        setBiomeHighAccuracy(settings.getBoolean("biomeHighAccuracy", false));
+        setBiomeInfos(tips.getStringList("biomeInfos"));
+        setBiomeSwitch(tips.getString("biomeSwitch", " 生物群系提示已%Action%!"));
+        setRegionSwitch(tips.getString("regionSwitch", " 区域提示已%Action%!"));
+    }
+
+    public static void reload() {
+        RegionInformationReload.getInstance().reloadConfig();
+        loadData();
+        reloadRegionsAndBiomes();
+    }
+
+    public static void loadRegionsAndBiomes() {
+        ConfigurationSection regions = config.getConfigurationSection("Regions");
+        regions.getKeys(false).stream().forEach(regionUniqueId -> {
+            ConfigurationSection region = regions.getConfigurationSection(regionUniqueId);
+            String type = region.getString("type");
+            if ("cube".equals(type)) {
+                DataManager.getRegions().add(new CubeRegion(
+                        regionUniqueId,
+                        region.getString("name"),
+                        region.getString("world"),
+                        "cube",
+                        region.getStringList("inInfos"),
+                        region.getStringList("outInfos"),
+                        new Point(region.getDouble("X1"),
+                                region.getDouble("Y1"),
+                                region.getDouble("Z1")),
+                        new Point(region.getDouble("X2"),
+                                region.getDouble("Y2"),
+                                region.getDouble("Z2"))
+                ));
+            } else if ("ball".equals(type)) {
+                DataManager.getRegions().add(new BallRegion(
+                        regionUniqueId,
+                        region.getString("name"),
+                        region.getString("world"),
+                        "ball",
+                        region.getStringList("inInfos"),
+                        region.getStringList("outInfos"),
+                        new Point(region.getDouble("centerX"),
+                                region.getDouble("centerY"),
+                                region.getDouble("centerZ")),
+                        region.getDouble("radius")
+                ));
+            } else {
+                DataManager.getRegions().add(new Region(region.getString("name"),
+                        region.getString("world"),
+                        "ERROR",
+                        regionUniqueId,
+                        region.getStringList("inInfos"),
+                        region.getStringList("outInfos")
+                ));
+            }
+        });
+        for (String i : biomes.keySet()) {
+            if (biomes.get(i) != null) {
+                DataManager.getBiomes().put(i, String.valueOf(biomes.get(i)));
+            } else {
+                DataManager.getBiomes().put(i, " ");
+            }
+        }
+    }
+
+    private static void reloadRegionsAndBiomes() {
+        regions.clear();
+        biomes.clear();
+        loadRegionsAndBiomes();
+    }
+
+    private static void saveRegion(Region newRegion) {
+        ConfigurationSection regions = config.getConfigurationSection("Regions");
+        regions.createSection(newRegion.getUniqueId());
+        ConfigurationSection newSectionOfRegion = regions.getConfigurationSection(newRegion.getUniqueId());
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("name", newRegion.getRegionName());
+        data.put("world", newRegion.getWorld());
+        data.put("type", newRegion.getType());
+
+        if (newRegion instanceof CubeRegion) {
+            Point point1 = ((CubeRegion) newRegion).getPoint1();
+            Point point2 = ((CubeRegion) newRegion).getPoint2();
+
+            data.put("X1", point1.getX());
+            data.put("Y1", point1.getY());
+            data.put("Z1", point1.getZ());
+            data.put("X2", point2.getX());
+            data.put("Y2", point2.getY());
+            data.put("Z2", point2.getZ());
+        } else if (newRegion instanceof BallRegion) {
+            Point center = ((BallRegion) newRegion).getCenter();
+
+            data.put("centerX", center.getX());
+            data.put("centerY", center.getY());
+            data.put("centerZ", center.getZ());
+            data.put("radius", ((BallRegion) newRegion).getRadius());
+        }
+
+        data.put("inInfos", newRegion.getInInfos());
+        data.put("outInfos", newRegion.getOutInfos());
+
+        for (Map.Entry<String, Object> entry : data.entrySet()) {
+            newSectionOfRegion.set(entry.getKey(), entry.getValue());
+        }
     }
 }
--- src/main/java/com/github/pregrafer/RegionInformationReload/RegionInformationReload.java
+++ src/main/java/com/github/pregrafer/RegionInformationReload/RegionInformationReload.java
@@ -1,11 +1,9 @@
 package com.github.pregrafer.RegionInformationReload;
 
 import com.github.pregrafer.RegionInformationReload.Listener.PlayerJoinAndQuit;
-import com.github.pregrafer.RegionInformationReload.Manager.AreaManager;
 import com.github.pregrafer.RegionInformationReload.Manager.DataManager;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -30,7 +28,8 @@ public class RegionInformationReload extends JavaPlugin {
         instance = this;
         saveDefaultConfig();
         register();
-        loadCS();
+        DataManager.loadData();
+        DataManager.loadRegionsAndBiomes();
         Bukkit.getLogger().info(ChatColor.translateAlternateColorCodes('&', DataManager.getPluginPrefix() + " &bLoading RegionInformationRe..."));
     }
 
@@ -44,18 +43,4 @@ public class RegionInformationReload extends JavaPlugin {
         Bukkit.getPluginManager().registerEvents(new PlayerJoinAndQuit(), instance);
     }
 
-    private void loadCS() {
-        config = getConfig();
-        ConfigurationSection settings = config.getConfigurationSection("Settings");
-        ConfigurationSection tips = config.getConfigurationSection("Tips");
-        DataManager.setPluginPrefix(settings.getString("pluginPrefix", "&7[&aRegionInfoRe&7]"));
-        DataManager.setBiomeSpeed(settings.getInt("biomeSpeed", 20));
-        DataManager.setRegionSpeed(settings.getInt("regionSpeed", 20));
-        DataManager.setBiomeHighAccuracy(settings.getBoolean("biomeHighAccuracy", false));
-        DataManager.setBiomeInfos(tips.getStringList("biomeInfos"));
-        DataManager.setBiomeSwitch(tips.getString("biomeSwitch", " 生物群系提示已%Action%!"));
-        DataManager.setRegionSwitch(tips.getString("regionSwitch", " 区域提示已%Action%!"));
-        DataManager.setAreaManager(new AreaManager());
-        DataManager.getAreaManager().loadRegionsAndBiomes();
-    }
 }
