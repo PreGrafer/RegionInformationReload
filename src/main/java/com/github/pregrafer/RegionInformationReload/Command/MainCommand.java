de5fab74b902bf701d9ec1d2b1814f94e64a2ecd
Kerwin <shijie.kerwin@qq.com>
2023-03-19 00:16:48 +0800

    command.java

---
+++ src/main/java/com/github/pregrafer/RegionInformationReload/Command/MainCommand.java
@@ -0,0 +1,24 @@
+package com.github.pregrafer.RegionInformationReload.Command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+
+import java.util.List;
+
+public class MainCommand implements CommandExecutor, TabExecutor {
+    public MainCommand() {
+        super();
+    }
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] strings) {
+        return null;
+    }
+}
